39026c45ecce8a8889af8c8fdfb7bd14cd8bfe10
Caioohv <caioviier@gmail.com>
2022-11-16 20:09:07 -0300

    fix: corrige implementação de cliente

--- src/main/java/com/mycompany/petshop/model/classes/Cliente.java
+++ src/main/java/com/mycompany/petshop/model/classes/Cliente.java
@@ -1,7 +1,31 @@
 package com.mycompany.petshop.model.classes;
 
+import java.util.ArrayList;
+
 public class Cliente {
   private ArrayList<Animal> animais;
   private String nome;
+
+    public Cliente(ArrayList<Animal> animais, String nome) {
+        this.animais = animais;
+        this.nome = nome;
+    }
+
+    public ArrayList<Animal> getAnimais() {
+        return animais;
+    }
+
+    public void setAnimais(ArrayList<Animal> animais) {
+        this.animais = animais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  
   
 }
