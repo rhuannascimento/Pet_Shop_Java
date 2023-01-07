b6720eb289879f06dada98f6a1fade8d99a89943
Lucas Duarte <lucasduartechaves@hotmail.com>
2023-01-07 17:32:46 -0300

    feat: cria model ração

---
+++ src/main/java/com/mycompany/petshop/model/classes/Racao.java
@@ -0,0 +1,35 @@
+package com.mycompany.petshop.model.classes;
+
+public class Racao extends Mercadoria {
+    private String especie;
+    private String sabor;
+
+    public Racao() {
+    }
+
+    public Racao(int estoque, String fornecedor, String categoria, int id, String nome, float preco, String tipo,
+            boolean disponivel, String especie, String sabor) {
+
+        super(estoque, fornecedor, categoria, id, nome, preco, tipo, disponivel);
+        this.especie = especie;
+        this.sabor = sabor;
+
+    }
+
+    public String getEspecie() {
+        return this.especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getSabor() {
+        return this.sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+}
