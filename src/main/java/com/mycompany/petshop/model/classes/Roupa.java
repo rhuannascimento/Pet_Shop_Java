9dc15d1f5306f6dbafa3bfa409dd19b31cb127f9
Lucas Duarte <lucasduartechaves@hotmail.com>
2023-01-07 17:33:04 -0300

    feat: cria model roupa

---
+++ src/main/java/com/mycompany/petshop/model/classes/Roupa.java
@@ -0,0 +1,36 @@
+package com.mycompany.petshop.model.classes;
+
+public class Roupa extends Mercadoria {
+
+    private String tamanho;
+    private String especie;
+
+    public Roupa() {
+    }
+
+    public Roupa(int estoque, String fornecedor, String categoria, int id, String nome, float preco, String tipo,
+            boolean disponivel, String tamanho, String especie) {
+
+        super(estoque, fornecedor, categoria, id, nome, preco, tipo, disponivel);
+        this.tamanho = tamanho;
+        this.especie = especie;
+
+    }
+
+    public String getTamanho() {
+        return this.tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getEspecie() {
+        return this.especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+}
