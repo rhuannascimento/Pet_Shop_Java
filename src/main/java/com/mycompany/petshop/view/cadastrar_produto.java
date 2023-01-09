92ec065550044c48d79f8eca92965dab8a6cb102
Beortizz <beortizz@gmail.com>
2023-01-09 05:09:18 -0300

    feat/pega dados para o cadastrod e produtos

--- src/main/java/com/mycompany/petshop/view/cadastrar_produto.java
+++ src/main/java/com/mycompany/petshop/view/cadastrar_produto.java
@@ -146,6 +146,9 @@ public class cadastrar_produto extends javax.swing.JPanel {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        String nome = jTextField1.getText();
+        String preco = jTextField2.getText();
+        String quantidade = jSpinner1.toString();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
