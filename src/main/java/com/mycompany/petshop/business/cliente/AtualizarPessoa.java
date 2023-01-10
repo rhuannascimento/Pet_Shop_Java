368341c6abcc41707409eeb2a4a8f3641d0d4c31
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-01-10 15:41:09 -0300

    Business/Fix: separando metodo de atualizar em duas classes(um pra pessoa e um pra animal)

--- src/main/java/com/mycompany/petshop/business/cliente/AtualizarPessoa.java
+++ src/main/java/com/mycompany/petshop/business/cliente/AtualizarPessoa.java
@@ -2,21 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.petshop.business.cliente;
-import com.mycompany.petshop.model.classes.Animal;
+
 import com.mycompany.petshop.model.classes.Cliente;
 import com.mycompany.petshop.model.classes.Pessoa;
 import com.mycompany.petshop.repository.ClienteRep;
-import java.sql.Time;
-import java.util.ArrayList;
+
 /**
  *
- * @author beort
+ * @author Rhuan
  */
-public class AtualizarCliente {
-    public AtualizarCliente (Cliente cliente){
+public class AtualizarPessoa {
+    public AtualizarPessoa (int id, String nome, String cpf, String email, String telefone){
         ClienteRep cr = new ClienteRep();
-        cr.update(cliente);
+        
+        Cliente pessoa = new Pessoa(id, nome, cpf, email, telefone);
+        
+        cr.update(pessoa);
     }
 }
