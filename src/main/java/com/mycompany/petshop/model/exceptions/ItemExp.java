5fe143b82bd90a0d192246ceb708e7200936a168
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-01-10 15:01:09 -0300

    Business/Implements: cadastrarServico, ListarProdutos, ListarServicos, ItemExp / Fix: private categoria em item e remove funcao get categoria de servico pois ja tem em item

---
+++ src/main/java/com/mycompany/petshop/model/exceptions/ItemExp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.petshop.model.exceptions;
+
+/**
+ *
+ * @author Rhuan
+ */
+public class ItemExp extends Exception{
+
+    public ItemExp(String message) {
+        super(message);
+    }
+    
+    
+    
+}
