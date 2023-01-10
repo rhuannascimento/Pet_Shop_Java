cb740526fd01241a951a686be8a03197c192bd2e
Beortizz <beortizz@gmail.com>
2023-01-10 18:58:03 -0300

    feat/cadastrar brinquedo

---
+++ src/main/java/com/mycompany/petshop/business/item/CadastrarBrinquedo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.petshop.business.item;
+import com.mycompany.petshop.model.classes.Item;
+import com.mycompany.petshop.model.classes.Brinquedo;
+import com.mycompany.petshop.repository.ItemRep;
+/**
+ *
+ * @author beort
+ */
+public class CadastrarBrinquedo {
+    public CadastrarBrinquedo(int estoque, String fornecedor, int id, String nome, float preco, String tipo,
+            boolean disponivel, String especie, String material){
+            
+        Item i;
+        i = new Brinquedo(estoque, fornecedor, id, nome, preco, tipo,disponivel,especie,material);
+        ItemRep ir = new ItemRep();
+        ir.insertOne(i);
+
+    }
+}
