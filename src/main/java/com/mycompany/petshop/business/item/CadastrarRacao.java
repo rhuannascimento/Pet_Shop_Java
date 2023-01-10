492cdacc171d5cdb3a22c0dc1b44a1edaac8b614
Beortizz <beortizz@gmail.com>
2023-01-10 18:49:45 -0300

    feat/atualizar racao e cadastro de remedio

--- src/main/java/com/mycompany/petshop/business/item/CadastrarRacao.java
+++ src/main/java/com/mycompany/petshop/business/item/CadastrarRacao.java
@@ -1,20 +1,17 @@
 package com.mycompany.petshop.business.item;
-import com.mycompany.petshop.model.classes.Animal;
 import com.mycompany.petshop.model.classes.Item;
-import com.mycompany.petshop.model.classes.Pessoa;
+import com.mycompany.petshop.model.classes.Racao;
 import com.mycompany.petshop.repository.ItemRep;
 import java.sql.Time;
 import java.util.ArrayList;
 
 public class CadastrarRacao {
     
-    public CadastrarRacao(int estoque, String fornecedor, String categoria, String nome, String sabor ,boolean disponivel, String especie,
-     float preco, String tipo, int id){
+    public CadastrarRacao(int estoque, String fornecedor, int id, String nome, float preco, String tipo,
+            boolean disponivel, String especie, String sabor, int idade_recomendada){
         Item i;
-      
-        i = new Item(estoque, fornecedor, categoria, id, nome, preco, tipo,disponivel,especie,sabor);
-        
-        ItemRep uir = new ItemRep();
+        i = new Racao(estoque, fornecedor, id, nome, preco, tipo,disponivel,especie,sabor, idade_recomendada);
+        ItemRep ir = new ItemRep();
        ir.insertOne(i);
     }
 }
